c3b05b50917cc6c7df0213b40f8653a8d357c89f
masonhorder <masonhorder@gmail.com>
2020-11-10 22:30:34 -0800

    updates

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+def divingBoard(longLength, shortLength, count):
+    returnList = []
+    totalCount = count + 1
+    
+    for a in range(totalCount):
+        miniList = []
+        longCount = 0
+        while longCount < a:
+            miniList.append(longLength)
+            longCount += 1
+        shortCount = 0
+        while shortCount < count-longCount:
+            miniList.append(shortLength)
+            shortCount += 1
+        print(miniList)
+        returnList.append(sum(miniList))
+        
+    
+    print("----------")
+    return returnList
+
+print(divingBoard(77,93,100))
+
+
