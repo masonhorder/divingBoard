595d23c2a1c090898b437dad1afbbd00c7c401b6
masonhorder <masonhorder@gmail.com>
2020-11-11 15:23:21 -0800

    final

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# def divingBoard(shortLength, longLength, count):
-#     returnList = []
-#     totalCount = count + 1
-#     for a in range(totalCount):
-#         returnList.append(shortLength*a + longLength*(count-a))
-#     return returnList
+def divingBoard(shortLength, longLength, count):
+    returnList = []
+    totalCount = count + 1
+    for a in range(totalCount):
+        returnList.append(shortLength*a + longLength*(count-a))
+    return returnList
 
-lengths = []
-shortLength = 1
-longLength = 2
-count = 3
-for a in range(count+1):
-    lengths.append(shortLength*a + longLength*(count-a))
+# lengths = []
+# shortLength = 1
+# longLength = 2
+# count = 3
+# for a in range(count+1):
+#     lengths.append(shortLength*a + longLength*(count-a))
 
 
-print(lengths)
+# print(lengths)
 
 
